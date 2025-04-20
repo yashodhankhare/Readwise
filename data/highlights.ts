be5d229280dccb31556dac5fd5d0af7d343d04bb
Yashodhan Khare <yashodhankhare@Yashodhans-MacBook-Pro---M1.local>
2025-04-20 10:50:52 +0530

    Fix: Add highlights.ts in correct project folder

---
+++ data/highlights.ts
@@ -0,0 +1,152 @@
+export const highlights = [
+  {
+    quote: `Personnel decisions are noisy. Interviewers of job candidates make widely different assessments of the same people. Performance ratings of the same employees are also highly variable and depend more on the person doing the assessment than on the performance being assessed.`,
+    book: "Noise: A Flaw in Human Judgment",
+    author: "Daniel Kahneman, Olivier Sibony, and Cass R. Sunstein",
+    tags: ["pink"]
+  },
+  {
+    quote: `Wherever you look at human judgments, you are likely to find noise. To improve the quality of our judgments, we need to overcome noise as well as bias.`,
+    book: "Noise: A Flaw in Human Judgment",
+    author: "Daniel Kahneman, Olivier Sibony, and Cass R. Sunstein",
+    tags: ["pink"]
+  },
+  {
+    quote: `Occasion noise is the variability in judgments of the same case by the same person or group on different occasions. A surprising amount of occasion noise arises in group discussion because of seemingly irrelevant factors, such as who speaks first.`,
+    book: "Noise: A Flaw in Human Judgment",
+    author: "Daniel Kahneman, Olivier Sibony, and Cass R. Sunstein",
+    tags: ["pink"]
+  },
+  {
+    quote: `The theme that emerges from these three chapters can be summarized in one sentence, which will be a key theme of this book: wherever there is judgment, there is noise—and more of it than you think. Let’s start to find out how much.`,
+    book: "Noise: A Flaw in Human Judgment",
+    author: "Daniel Kahneman, Olivier Sibony, and Cass R. Sunstein",
+    tags: ["pink"]
+  },
+  {
+    quote: `To understand error in judgment, we must understand both bias and noise. Sometimes, as we will see, noise is the more important problem. But in public conversations about human error and in organizations all over the world, noise is rarely recognized. Bias is the star of the show. Noise is a bit player, usually offstage. The topic of bias has been discussed in thousands of scientific articles and dozens of popular books, few of which even mention the issue of noise. This book is our attempt to redress the balance. In real-world decisions, the amount of noise is often scandalously high.`,
+    book: "Noise: A Flaw in Human Judgment",
+    author: "Daniel Kahneman, Olivier Sibony, and Cass R. Sunstein",
+    tags: ["pink"]
+  },
+  {
+    quote: `general property of noise is that`,
+    book: "Noise: A Flaw in Human Judgment",
+    author: "Daniel Kahneman, Olivier Sibony, and Cass R. Sunstein",
+    tags: ["pink"]
+  },
+  {
+    quote: `you can recognize and measure it while knowing nothing about the target or bias.`,
+    book: "Noise: A Flaw in Human Judgment",
+    author: "Daniel Kahneman, Olivier Sibony, and Cass R. Sunstein",
+    tags: ["pink"]
+  },
+  {
+    quote: `Medicine is noisy. Faced with the same patient, different doctors make different judgments about whether patients have skin cancer, breast cancer, heart disease, tuberculosis, pneumonia, depression, and a host of other conditions. Noise is especially high in psychiatry, where subjective judgment is obviously important. However, considerable noise is also found in areas where it might not be expected, such as in the reading of X-rays.`,
+    book: "Noise: A Flaw in Human Judgment",
+    author: "Daniel Kahneman, Olivier Sibony, and Cass R. Sunstein",
+    tags: ["pink"]
+  },
+  {
+    quote: `Forecasts are noisy. Professional forecasters offer highly variable predictions about likely sales of a new product, likely growth in the unemployment rate, the likelihood of bankruptcy for troubled companies, and just about everything else. Not only do they disagree with each other, but they also disagree with`,
+    book: "Noise: A Flaw in Human Judgment",
+    author: "Daniel Kahneman, Olivier Sibony, and Cass R. Sunstein",
+    tags: ["pink"]
+  },
+  {
+    quote: `themselves. For example, when the same software developers were asked on two separate days to estimate the completion time for the same task, the hours they projected differed by 71%, on average.`,
+    book: "Noise: A Flaw in Human Judgment",
+    author: "Daniel Kahneman, Olivier Sibony, and Cass R. Sunstein",
+    tags: ["pink"]
+  },
+  {
+    quote: `Let the little girl see the role of the female and she will grow up to be a woman with a natural affection for the opposite sex.`,
+    book: "SEE YOU AT THE TOP",
+    author: "Zig Ziglar",
+    tags: ["parenting"]
+  },
+  {
+    quote: `say, they get cooked in the squat.`,
+    book: "SEE YOU AT THE TOP",
+    author: "Zig Ziglar",
+    tags: ["discard"]
+  },
+  {
+    quote: `He was another in the endless list of thieves who steal from themselves when they try to steal from others.`,
+    book: "SEE YOU AT THE TOP",
+    author: "Zig Ziglar",
+    tags: ["mindset"]
+  },
+  {
+    quote: `Simply stated, it means that when your image improves, your performance improves.`,
+    book: "SEE YOU AT THE TOP",
+    author: "Zig Ziglar",
+    tags: ["discard"]
+  },
+  {
+    quote: `plate and never really take a cut at the ball. He is the biggest failure of all, according to Larry Kimsey, M.D., because he doesn’t try.`,
+    book: "SEE YOU AT THE TOP",
+    author: "Zig Ziglar",
+    tags: ["discard"]
+  },
+  {
+    quote: `You must feel you “deserve” success, happiness, etc.,`,
+    book: "SEE YOU AT THE TOP",
+    author: "Zig Ziglar",
+    tags: ["discard"]
+  },
+  {
+    quote: `One point you need to clearly understand is that education and intelligence are not the same thing.`,
+    book: "SEE YOU AT THE TOP",
+    author: "Zig Ziglar",
+    tags: ["favorite,what intelligence tests miss"]
+  },
+  {
+    quote: `Take what you have and use it and your talent will be increased, which brings more rewards, etc.`,
+    book: "SEE YOU AT THE TOP",
+    author: "Zig Ziglar",
+    tags: ["discard"]
+  },
+  {
+    quote: `As individuals, we will consistently act according to the way we see ourselves.`,
+    book: "SEE YOU AT THE TOP",
+    author: "Zig Ziglar",
+    tags: ["discard"]
+  },
+  {
+    quote: `Any relationship primarily built on physical attractiveness is predestined to be short-lived.`,
+    book: "SEE YOU AT THE TOP",
+    author: "Zig Ziglar",
+    tags: ["discard"]
+  },
+  {
+    quote: `the male side of the table, I agree with her one hundred percent.`,
+    book: "SEE YOU AT THE TOP",
+    author: "Zig Ziglar",
+    tags: ["discard"]
+  },
+  {
+    quote: `more than you can possibly give. In so many cases, what you give will mean much to the recipient, but the feeling you get when you do something for someone who cannot do for himself is indescribable.`,
+    book: "SEE YOU AT THE TOP",
+    author: "Zig Ziglar",
+    tags: ["favorite,gratitude"]
+  },
+  {
+    quote: `Charles Dickens said it best: “No one is useless in this world who lightens the burden of it to anyone else.”`,
+    book: "SEE YOU AT THE TOP",
+    author: "Zig Ziglar",
+    tags: ["empathy"]
+  },
+  {
+    quote: `Shakespeare said it, “This above all, to thine own self be true. And it must follow as the night the day, thou canst not then be false to any man.”`,
+    book: "SEE YOU AT THE TOP",
+    author: "Zig Ziglar",
+    tags: ["discard"]
+  },
+  {
+    quote: `you feel inferior without your permission.`,
+    book: "SEE YOU AT THE TOP",
+    author: "Zig Ziglar",
+    tags: ["discard"]
+  }
+];
