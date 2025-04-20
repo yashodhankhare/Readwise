7499b4394a4868711898834b006879da6e0df4f6
Yashodhan Khare <yashodhankhare@Yashodhans-MacBook-Pro---M1.local>
2025-04-20 11:46:44 +0530

    Fix: Clean up page.tsx and add cleaned highlights data

--- app/page.tsx
+++ app/page.tsx
@@ -4,25 +4,6 @@ import { highlights } from "../data/highlights";
 
 
 
-
-// 👋 Trigger redeploy for latest homepage
-
-
-const highlights = [
-  {
-    quote: "You don't have to be sure to make a good decision. You just need a good process.",
-    author: "Annie Duke",
-    book: "Thinking in Bets",
-    tags: ["decision-making", "mental models"]
-  },
-  {
-    quote: "Markets are not random. They are based on human behavior...",
-    author: "Jack Schwager",
-    book: "Market Wizards",
-    tags: ["trading", "psychology"]
-  }
-];
-
 export default function Home() {
   return (
     <main style={{ padding: "2rem" }}>
